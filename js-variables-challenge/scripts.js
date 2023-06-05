43713b2329d4db5ff5356fb219387f4756f81b3c
Janet Suryadi <janet@suryadi.id.au>
2023-06-05 11:28:54 +1000

    Completed the challenge

--- js-variables-challenge/scripts.js
+++ js-variables-challenge/scripts.js
@@ -5,7 +5,11 @@
 
 /*----------  Variable Declaration  ----------*/
 
-
+let firstName = 'Janet';
+let lastName = 'Suryadi';
+let age = 22;
 
 /*----------  Script assignment  ----------*/
+
+console.log(`Hi, my name is ${firstName} ${lastName} and I'm ${age} years old`);
 
