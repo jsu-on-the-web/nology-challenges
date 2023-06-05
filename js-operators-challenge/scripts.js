5f890fdb46131efc94f8523c71f6a16ef9da1734
Janet Suryadi <janet@suryadi.id.au>
2023-06-05 12:30:37 +1000

    Age challenge

--- js-operators-challenge/scripts.js
+++ js-operators-challenge/scripts.js
@@ -4,7 +4,15 @@
 
 /*----------  Variable Declaration  ----------*/
 
-
+let age = 22;
 
 /*----------  Script assignment  ----------*/
 
+age += 10;
+console.log(`${age}`);
+
+age -= 7;
+console.log(`${age}`);
+
+age *= 11;
+console.log(`${age}`);
