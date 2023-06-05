48e1a73716aa7faa7da6387b8347c679e94f2548
Janet Suryadi <janet@suryadi.id.au>
2023-06-05 12:38:00 +1000

    Moon rotation challenge

--- js-operators-challenge/scripts.js
+++ js-operators-challenge/scripts.js
@@ -5,6 +5,9 @@
 /*----------  Variable Declaration  ----------*/
 
 let age = 22;
+let earthDays = 365;
+let moonRotationPeriod = 27;
+let moonRotationsInEarthDays = earthDays / moonRotationPeriod;
 
 /*----------  Script assignment  ----------*/
 
@@ -16,3 +19,7 @@ console.log(`${age}`);
 
 age *= 11;
 console.log(`${age}`);
+
+
+// Calculating Moon Rotations --------------------------------
+console.log(`The moon will complete ${moonRotationsInEarthDays} rotations in ${earthDays} days`);
