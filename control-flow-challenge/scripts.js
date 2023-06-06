3b67c676f5f289aacb1432c5ca60d5bc0dcae3ab
Janet Suryadi <janet@suryadi.id.au>
2023-06-06 11:53:41 +1000

    Challenge 2

--- control-flow-challenge/scripts.js
+++ control-flow-challenge/scripts.js
@@ -8,6 +8,8 @@
 let x = 1;
 let y = 2;
 
+let userInput = prompt('Please enter some input');
+
 /*----------  Script assignment  ----------*/
 
 // Challenge 1
@@ -19,3 +21,11 @@ if (x < y) {
     console.log("x is equal to y");
 }
 
+// Challenge 2 
+if (parseFloat(userInput) || parseInt(userInput)) {
+    console.log(`The input squared is ${userInput ** 2}`);
+} else if (typeof userInput === 'string') {
+    console.log(`The input is ${userInput}`);
+} else {
+    console.log("Invalid input");
+}
