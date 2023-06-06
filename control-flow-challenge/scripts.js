4659134dbdf2626a728fb49fb4a5eb80ad835387
Janet Suryadi <janet@suryadi.id.au>
2023-06-07 09:10:59 +1000

    Little bit of challenge 3 and 4

--- control-flow-challenge/scripts.js
+++ control-flow-challenge/scripts.js
@@ -10,6 +10,11 @@ let y = 2;
 
 let userInput = prompt('Please enter some input');
 
+let day = 3;
+
+let parent1 = { eyeColour: 'green' };
+let parent2 = { eyeColour: 'brown' };
+
 /*----------  Script assignment  ----------*/
 
 // Challenge 1
@@ -22,10 +27,13 @@ if (x < y) {
 }
 
 // Challenge 2 
-if (parseFloat(userInput) || parseInt(userInput)) {
+if (parseFloat(userInput) !== NaN || parseInt(userInput) !== NaN) {
     console.log(`The input squared is ${userInput ** 2}`);
 } else if (typeof userInput === 'string') {
     console.log(`The input is ${userInput}`);
 } else {
     console.log("Invalid input");
 }
+
+
+//Challenge 3 
