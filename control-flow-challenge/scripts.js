4f060402e3391616674f824089d17d2494c901a9
Janet Suryadi <janet@suryadi.id.au>
2023-06-07 09:23:09 +1000

    Challenge 3

--- control-flow-challenge/scripts.js
+++ control-flow-challenge/scripts.js
@@ -37,3 +37,29 @@ if (parseFloat(userInput) !== NaN || parseInt(userInput) !== NaN) {
 
 
 //Challenge 3 
+switch (day) {
+    case 1:
+        console.log(`Today is Monday`);
+        break;
+    case 2:
+        console.log(`Today is Tuesday`);
+        break;
+    case 3:
+        console.log(`Today is Wednesday`);
+        break;
+    case 4:
+        console.log(`Today is Thursday`);
+        break;
+    case 5:
+        console.log(`Today is Friday`);
+        break;
+    case 6:
+        console.log(`Today is Saturday`);
+        break;
+    case 7:
+        console.log(`Today is Sunday`);
+        break;
+    default:
+        console.log(`Unknown day`);
+        break;
+}
