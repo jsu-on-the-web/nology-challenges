1417cfb78cba4ff815abd5d269415cad1176f5d9
Janet Suryadi <janet@suryadi.id.au>
2023-06-06 11:48:00 +1000

    Challenge 1

--- control-flow-challenge/scripts.js
+++ control-flow-challenge/scripts.js
@@ -5,7 +5,17 @@
 
 /*----------  Variable Declaration  ----------*/
 
-
+let x = 1;
+let y = 2;
 
 /*----------  Script assignment  ----------*/
+
+// Challenge 1
+if (x < y) {
+    console.log("x is smaller than y");
+} else if (x > y) {
+    console.log("x is greater than y");
+} else {
+    console.log("x is equal to y");
+}
 
