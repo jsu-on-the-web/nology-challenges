6abbcd9fcfb813b5fe647734a4c619d07e97fd8d
Janet Suryadi <janet@suryadi.id.au>
2023-06-07 10:44:05 +1000

    Challenge 4

--- functions-challenge/scripts.js
+++ functions-challenge/scripts.js
@@ -28,6 +28,9 @@ const ageInSeconds = function (age) {
     return age * 31536000;
 }
 
+const remainder = function (numberOne, numberTwo) {
+    return numberOne % numberTwo;
+}
 
 /*----------  Variable Declaration  ----------*/
 
@@ -35,12 +38,17 @@ let r = 5;
 let earthDays = 54;
 let myAge = 22;
 
+let n1 = 24;
+let n2 = 5;
+
 
 /*----------  Script assignment  ----------*/
 
 
 console.log(`The moon will orbit around the earth around ${moonOrbits(earthDays)} times in ${earthDays} days.`);
 
-console.log(`The circle's area is about ${circleArea(r)} and its perimeter is about ${circlePerimeter(r)}.`);
+console.log(`The circle's radius is ${r}, so its area is about ${circleArea(r)} and its perimeter is about ${circlePerimeter(r)}.`);
 
 console.log(`You're ${myAge} years old, so that means you're ${ageInDays(myAge)} days old and ${ageInSeconds(myAge)} seconds old.`);
+
+console.log(`If you divide ${n1} by ${n2}, you'll get ${remainder(n1, n2)} as a remainder.`);
