0212a5b7e9b56d4b1a3a9b1f6d786a80586a9d7d
Janet Suryadi <janet@suryadi.id.au>
2023-06-07 10:04:35 +1000

    Challenge 4 minus bonus

--- control-flow-challenge/scripts.js
+++ control-flow-challenge/scripts.js
@@ -14,6 +14,11 @@ let day = 3;
 
 let parent1 = { eyeColour: 'green' };
 let parent2 = { eyeColour: 'brown' };
+let parentEyecolours = parent1.eyeColour + parent2.eyeColour;
+let child = { eyeColour: '' };
+let blueChance = 0;
+let greenChance = 0;
+let brownChance = 0;
 
 /*----------  Script assignment  ----------*/
 
@@ -63,3 +68,47 @@ switch (day) {
         console.log(`Unknown day`);
         break;
 }
+
+// Challenge 4
+
+parentEyecolours = parentEyecolours.toLowerCase();
+
+switch (parentEyecolours) {
+    case "brownbrown":
+        brownChance = 75;
+        greenChance = 18.8;
+        blueChance = 6.3;
+        break;
+    case "brownblue":
+    case "brownblue":
+        brownChance = 50;
+        greenChance = 0;
+        blueChance = 50;
+        break;
+    case "browngreen":
+    case "greenbrown":
+        brownChance = 50;
+        greenChance = 37.5;
+        blueChance = 12.5;
+        break;
+    case "bluegreen":
+    case "greenblue":
+        brownChance = 0;
+        greenChance = 50;
+        blueChance = 50;
+        break;
+    case "greengreen":
+        brownChance = 0;
+        greenChance = 75;
+        blueChance = 25;
+        break;
+    case "blueblue":
+        brownChance = 0;
+        greenChance = 1;
+        blueChance = 99;
+        break;
+}
+
+console.log(`You and your partner have ${parent1.eyeColour} and ${parent2.eyeColour} eyes. So your child will have a ${blueChance}% chance of having blue eyes, a ${greenChance}% chance of having green eyes, and a ${brownChance}% chance of having brown eyes.`);
+
+// child.eyeColour = Math.random() * 100; // Random number between 0 and 100, excluding 100.
