4fbb3b2eb175ce1fbfc1019045454a4670056ba2
Janet Suryadi <janet@suryadi.id.au>
2023-06-07 10:41:41 +1000

    Challenge 3

--- functions-challenge/scripts.js
+++ functions-challenge/scripts.js
@@ -20,10 +20,20 @@ const circlePerimeter = function (radius) {
     return (2 * Math.PI * radius).toFixed(2);
 }
 
+const ageInDays = function (age) {
+    return age * 365;
+}
+
+const ageInSeconds = function (age) {
+    return age * 31536000;
+}
+
+
 /*----------  Variable Declaration  ----------*/
 
 let r = 5;
-let earthDays = 55;
+let earthDays = 54;
+let myAge = 22;
 
 
 /*----------  Script assignment  ----------*/
@@ -32,3 +42,5 @@ let earthDays = 55;
 console.log(`The moon will orbit around the earth around ${moonOrbits(earthDays)} times in ${earthDays} days.`);
 
 console.log(`The circle's area is about ${circleArea(r)} and its perimeter is about ${circlePerimeter(r)}.`);
+
+console.log(`You're ${myAge} years old, so that means you're ${ageInDays(myAge)} days old and ${ageInSeconds(myAge)} seconds old.`);
