8bd6353abc80ff12c7664ec99e760f5e8ac1cf11
Janet Suryadi <janet@suryadi.id.au>
2023-06-07 10:35:26 +1000

    Challenge 1 cleanup and Challengw 2

--- functions-challenge/scripts.js
+++ functions-challenge/scripts.js
@@ -2,17 +2,33 @@
 
 const moonOrbits = function (earthDays) {
     const moonOrbitPeriod = 27;
-    return (earthDays / moonOrbitPeriod).toFixed(3);
+
+    const totalOrbits = earthDays / moonOrbitPeriod;
+
+    // if (earthDays % moonOrbitPeriod === 0) return totalOrbits;
+    // else return totalOrbits.toFixed(3);
+
+    return earthDays % moonOrbitPeriod === 0 ? totalOrbits : totalOrbits.toFixed(3);
+
 }
 
+const circleArea = function (radius) {
+    return (Math.PI * radius ** 2).toFixed(2);
+}
+
+const circlePerimeter = function (radius) {
+    return (2 * Math.PI * radius).toFixed(2);
+}
 
 /*----------  Variable Declaration  ----------*/
 
-
+let r = 5;
+let earthDays = 55;
 
 
 /*----------  Script assignment  ----------*/
 
 
-console.log(moonOrbits(54));
-console.log(moonOrbits(365));
+console.log(`The moon will orbit around the earth around ${moonOrbits(earthDays)} times in ${earthDays} days.`);
+
+console.log(`The circle's area is about ${circleArea(r)} and its perimeter is about ${circlePerimeter(r)}.`);
