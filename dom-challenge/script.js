77ef1682c513ae50f05ade675c2d6c4e27b398b9
Janet Suryadi <janet@suryadi.id.au>
2023-06-16 10:42:05 +1000

    More work on DOM challenge

--- dom-challenge/script.js
+++ dom-challenge/script.js
@@ -1,7 +1,8 @@
 /*----------  Function Declaration  ----------*/
 const pubCheck = function (age, country, drunkLevel) {
     let canEnterPub = false;
-    document.removeChild(message);
+    message.hidden = true;
+    message.classList.remove(['message--success', 'message--failure']);
 
     // First is the country check since different countries have different drinking ages
     switch (country) {
@@ -21,7 +22,25 @@ const pubCheck = function (age, country, drunkLevel) {
     }
 
     // Message to show check
-
+    if (document.querySelector("#age").nodeValue === null && document.querySelector("#drunk").nodeValue === null) {
+        btn.style.backgroundColor = "#ffbd4a";
+        beerBottle.src = "assets/beer.svg";
+        message.hidden = true;
+        return;
+    }
+    if (canEnterPub === true) {
+        beerBottle.src = "assets/green_beer.svg";
+        btn.style.backgroundColor = "#008000";
+        message.classList.add("message--success");
+        message.innerHTML = "Come in and have a beer";
+        message.hidden = false;
+    } else {
+        message.innerHTML = "You can't have a beer";
+        btn.style.backgroundColor = "#b40f0f";
+        message.classList.add("message--failure");
+        beerBottle.src = "assets/red_beer.svg";
+        message.hidden = false;
+    }
 
     return canEnterPub;
 }
@@ -31,9 +50,11 @@ const pubCheck = function (age, country, drunkLevel) {
 
 /*----------  Variable Declaration  ----------*/
 let message = document.getElementById("message");
-
+let btn = document.getElementById("btn");
+let beerBottle = document.querySelector("img");
 
 /*----------  Script assignment  ----------*/
+btn.addEventListener("submit", pubCheck(document.querySelector("#age").nodeValue, document.querySelector("#country").value, document.querySelector("#drunk").nodeValue));
 
 
 
