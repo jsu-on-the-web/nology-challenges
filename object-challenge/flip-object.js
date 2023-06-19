6ae4ef3b17f2949968dceb87a42a2266777c21d2
Janet Suryadi <janet@suryadi.id.au>
2023-06-19 12:28:16 +1000

    flip object done

--- object-challenge/flip-object.js
+++ object-challenge/flip-object.js
@@ -6,6 +6,16 @@ const translate = {
     strawberry: "truskawka",
 };
 
+function flipKeysAndValues(obj) {
+    const entries = Object.entries(obj);
+    const flippedEntries = entries.map(entry => entry.reverse());
+    const output = Object.fromEntries(flippedEntries);
+    return output;
+}
+
+console.log(translate);
+console.log(flipKeysAndValues(translate));
+
 /* const newObj = {
     jablko: "apple",
     gruszka: "pear",
