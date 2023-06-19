02469d450e9fe0fddafd6b593b8f7f7f8b315758
Janet Suryadi <janet@suryadi.id.au>
2023-06-19 11:22:25 +1000

    starting challenge

---
+++ object-challenge/flip-object.js
@@ -0,0 +1,13 @@
+// Swap keys with values
+
+const translate = {
+    apple: "jablko",
+    pear: "gruszka",
+    strawberry: "truskawka",
+};
+
+/* const newObj = {
+    jablko: "apple",
+    gruszka: "pear",
+    truskawka: "strawberry"
+} */
