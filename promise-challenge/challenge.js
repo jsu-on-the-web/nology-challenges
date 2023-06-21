41933ae64feb8e69f244997d872626e98c5b00a4
Janet Suryadi <janet@suryadi.id.au>
2023-06-21 10:57:02 +1000

    Testing the function. Challenge success

--- promise-challenge/challenge.js
+++ promise-challenge/challenge.js
@@ -4,14 +4,17 @@ const delayedIncDec = (n, mode, delay = 2500) => {
         setTimeout(() => {
             if (mode === "inc") {
                 n++;
-                resolve(`Incremented to ${n}`);
+                resolve(`Incremented to ${n} after ${delay}ms`);
             } else if (mode === "dec") {
                 n--;
-                resolve(`Decremented to ${n}`);
+                resolve(`Decremented to ${n} after ${delay}ms`);
             } else {
-                reject(`Invalid mode ${mode}`);
+                reject(`Invalid mode: ${mode}`);
             }
         }, delay);
     })
 }
 
+delayedIncDec(5, "inc", 1000).then((value) => { console.log(value) }).catch(err => { console.log(err) });
+delayedIncDec(20, "dec", 3000).then((value) => { console.log(value) }).catch(err => { console.log(err) });
+delayedIncDec(520, "inc").then((value) => { console.log(value) }).catch(err => { console.log(err) });
