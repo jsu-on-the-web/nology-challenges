a7d2310e1e66fd380b0df7120a4c9c34bb512a65
Janet Suryadi <janet@suryadi.id.au>
2023-06-23 11:14:13 +1000

    rectangleArea method

---
+++ testing-challenge/code-along/shapes.js
@@ -0,0 +1,27 @@
+// Given two valid numbers, return the the area of a rectangle with the values
+/** 
+ * What could go wrong?
+ * 1. Invalid parameters
+ *    Make sure that the values are positive, n < 0 -> throw an error
+ * 2. Parameters are not numbers
+ *    Make sure that the values are numbers, typeof n !== 'number'  -> throw an error
+ * 3. User doesn't pass all the parameters
+ *    The user should pass both a width and height. If only one parameter is passed -> throw an error
+*/
+
+//Non TDD approach
+export const rectangleArea = (width, height) => {
+    if (height < 0 || width < 0) {
+        throw new Error("Height and width must be greater than zero");
+    }
+
+    if (typeof width !== 'number' || typeof height !== 'number') {
+        throw new Error("Width and height must be numbers");
+    }
+
+    if (height === undefined || width === undefined) {
+        throw new Error("One or more parameters are missing");
+    }
+
+    return height * width;
+}
--- testing-challenge/code-along/shapes.test.js
+++ testing-challenge/code-along/shapes.test.js
@@ -1,4 +1,12 @@
+const shapes = require('./shapes')
+
 // Test 1: Sanity check; Did I set everything up correctly?
 test('Is Jest setup correctly?', () => {
     expect(true).toBe(true);
 });
+
+describe('rectangleArea tests', () => {
+    test('The parameters are positive numbers', () => {
+
+    })
+})
