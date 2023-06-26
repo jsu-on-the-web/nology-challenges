913773df50de2edcb03b525ce99b2be98f8a7ed0
Janet Suryadi <janet@suryadi.id.au>
2023-06-27 09:18:09 +1000

    Misc React learning

---
+++ React/code-along/code-along-project/src/Card.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+import React, { Component } from 'react'
+
+export function Card() {
+  return (
+    <div>Card</div>
+  )
+}
---
+++ React/code-along/code-along-project/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function Header() {
+  return (
+    <header>
+          <h1>React challenge</h1>
+          <p>This is our first React challenge.</p>
+    </header>
+  )
+}
