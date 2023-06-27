073d787ef8edc8d1aaccaf0ff4e3bfb864ebd0e4
Janet Suryadi <janet@suryadi.id.au>
2023-06-27 10:44:01 +1000

    Centering and header

--- React/my-favourite-albums-challenge/src/App.jsx
+++ React/my-favourite-albums-challenge/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 
-import './App.css'
+import './App.scss'
 import { Card } from './Card/Card'
+import { Header } from './Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
+  const flexWrap = {
+    display: 'flex',
+    flexWrap: true,
+    justifyContent: 'center',
+  };
 
   return (
     <>
-      <Card albumTitle='Smile' albumArtist='Eve' albumRating={4} albumCover="../src/assets/album_covers/Smile_-_cover.png" />
-      <Card albumTitle='LOVE' albumArtist='PinocchioP' albumRating={5} albumCover="../src/assets/album_covers/love_cover.jpg" />
-      <Card albumTitle='Operation Mindcrime' albumArtist='Queensryche' albumRating={4} albumCover="../src/assets/album_covers/Queensryche_-_Operation_Mindcrime_cover.jpg" />
+      <Header />
+      <section className='card-gallery' style={flexWrap}>
+        <Card albumTitle='Smile' albumArtist='Eve' albumRating={4} albumCover="../src/assets/album_covers/Smile_-_cover.png" />
+        <Card albumTitle='LOVE' albumArtist='PinocchioP' albumRating={5} albumCover="../src/assets/album_covers/love_cover.jpg" />
+        <Card albumTitle='Operation Mindcrime' albumArtist='Queensryche' albumRating={4} albumCover="../src/assets/album_covers/Queensryche_-_Operation_Mindcrime_cover.jpg" />
+      </section>
     </>
 
   )
---
+++ React/my-favourite-albums-challenge/src/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.scss'
+export function Header() {
+    return (
+        <header>
+            <h1>My Favourite Albums</h1>
+            <p>This is our first React challenge.</p>
+        </header>
+    )
+}
