d59757e2cd075cb4f8bc785c95ad1f228e53d11c
Janet Suryadi <janet@suryadi.id.au>
2023-06-27 10:28:26 +1000

    Progress

---
+++ React/my-favourite-albums-challenge/src/Card/Card.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import './Card.scss'
+
+export function Card({ albumTitle = 'Album Title', albumArtist = 'Artist', albumRating = 0, albumCover }) {
+    return (
+        <section className="card" style={{ backgroundImage: `url(${albumCover})` }}>
+            <p className='card__album-title'>{albumTitle}</p>
+            <p className='card__album-artist'>{albumArtist}</p>
+            <p className='card__album-rating'>{albumRating}</p>
+
+        </section>
+    )
+}
